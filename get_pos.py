afd9fb317a64a54a6e75409c2ad5f886ef0a41d1
Florian Pfeiffer <bitbucket@x8s.de>
2018-01-12 09:01:24 +0100

    added get_pos example

---
+++ get_pos.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("/usr/local/lib64/python2.7/site-packages/")
+
+import math
+from playerc import *
+
+# Create a client object
+c = playerc_client(None, 'rockrobo', 6665)
+# Connect it
+if c.connect() != 0:
+        print("Can't connect")
+        sys.exit(0)
+
+p = playerc_position2d(c,0)
+if p.subscribe(PLAYERC_OPEN_MODE) != 0:
+        print("Can't subscribe")
+        sys.exit(0)
+
+c.read()
+print(p.px,p.py)
